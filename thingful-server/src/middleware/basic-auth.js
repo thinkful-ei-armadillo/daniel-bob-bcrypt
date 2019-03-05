e1b874f154338721b4dd41606574fec1ca5b9015
bobnearents <bobnearents@yahoo.com>
2019-03-05 08:19:35 -0800

    starting point for bcrpyt assignment

---
+++ thingful-server/src/middleware/basic-auth.js
@@ -0,0 +1,36 @@
+'use strict';
+
+function requireAuth(req, res, next) {
+  const authToken = req.get('Authorization') || '';
+  console.log('requireAuth is working')
+  let bearerToken;
+  if (!authToken.toLowerCase().startsWith('bearer ')) {
+    return res.status(401).json({error: 'Missing bearer token'});
+  } else {
+    bearerToken = authToken.slice(7, authToken.length)
+  }
+
+  const [tokenUserName, tokenPassword] = Buffer 
+    .from(bearerToken, 'base64')
+    .toString()
+    .split(':');
+
+  if (!tokenUserName || !tokenPassword) {
+    return res.status(401).json({error: 'Unautorized request'});
+  }
+  req.app.get('db')('thingful_users')
+    .where({ user_name: tokenUserName})
+    .first()
+    .then(user => {
+      if(!user){
+        return res.status(401).json({ error: 'Unauthorized request'})
+      }
+      req.user = user
+      next()
+    })
+    .catch(next)
+}
+
+module.exports = {
+  requireAuth,
+};
